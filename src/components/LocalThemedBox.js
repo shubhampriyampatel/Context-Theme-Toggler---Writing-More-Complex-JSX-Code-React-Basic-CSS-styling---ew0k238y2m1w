447eab108883bc5c94fb293f90a2e1e2f7586ee0
Jay Kumar Bharti <jaybharti362@gmail.com>
2023-01-17 18:27:52 +0530

    fix bug

--- src/components/LocalThemedBox.js
+++ src/components/LocalThemedBox.js
@@ -8,7 +8,7 @@ const LocalThemedBox = () => {
         <div className={`bg-${bg}`} style={{width:'200px',height:'200px',border:'2px solid green'}} id="local-themed-box">
             {/* Write code below this line */}
             <p id="local-themed-text-container" className={`txt-${bg}`}>Hii</p>
-            <button className={`btn btn-${bg}`} id="local-theme-toggler" onClick={()=>changeBg(bg==='light'? 'dark' : 'light')}>Toggle local theme to {bg==='light'? 'dark' : 'light'}</button>
+            <button className={`btn btn-${bg} txt-${bg}`} id="local-theme-toggler" onClick={()=>changeBg(bg==='light'? 'dark' : 'light')}>Toggle local theme to {bg==='light'? 'dark' : 'light'}</button>
         </div>
     )
     }
