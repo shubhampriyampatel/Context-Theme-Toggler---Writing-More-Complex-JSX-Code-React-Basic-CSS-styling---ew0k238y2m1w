3364702403f1aac6b818ade39a57ab95bd65656d
Jay Kumar Bharti <jaybharti362@gmail.com>
2023-01-17 18:20:38 +0530

    fix bug

--- src/components/Page.js
+++ src/components/Page.js
@@ -6,7 +6,7 @@ const Page = () => {
     const {globalBg} = React.useContext(ThemeContext)
     return(
         <div className={`container bg-${globalBg}`} id="themed-page">
-            <p id="themed-text-container " className={`txt-${globalBg}`}>
+            <p id="themed-text-container" className={`txt-${globalBg}`}>
                 lorem ipsum dolor iterit n stuff
             </p>
             <button className={`btn btn-${globalBg}`} id="themed-button">Themed Button</button>
